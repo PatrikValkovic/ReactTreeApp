8c0ddde5ead95c0e8f0a123c39bdbca44bf546e0
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-07 23:19:09 +0100

    Removed logs

--- app/Components/TaskContainer.js
+++ app/Components/TaskContainer.js
@@ -13,18 +13,15 @@ import nodesStore from '../Flux/nodesStore'
 
 class TaskContainer extends Component {
     static getStores(){
-        console.log('getStores')
         return [formStore, nodesStore]
     }
     static calculateState(prevState){
-        console.log('calcualte')
         return {
             form: formStore.getState(),
             nodes: nodesStore.getState()
         }
     }
     render() {
-        console.log(this.state)
         return (
             <TreeNode data={this.state.nodes}/>
         )
