e86a68dae57a9702df6e1feace865992cb9b1e71
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-07 16:34:47 +0100

    Add basic styling

--- app/TreeNode.js
+++ app/TreeNode.js
@@ -4,18 +4,38 @@
  */
 
 'use strict'
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 
 export default class TreeNode extends Component {
-    render(){
-        const childs = this.props.data.childs.map((child) => {
-            return <TreeNode data={child} key={child.id}/>
+
+    createStyle() {
+        const obj = {}
+        if (!this.props.marginLeft)
+            obj.marginLeft = 0
+        if (!this.props.marginRight)
+            obj.marginRight = 0
+        return obj
+    }
+
+    createChilds() {
+        const childLength = this.props.data.childs.length
+        return this.props.data.childs.map((child, childIndex) => {
+            return (<TreeNode data={child}
+                              key={child.id}
+                              marginLeft={childIndex !== 0}
+                              marginRight={childIndex !== childLength - 1}/>)
         })
+    }
+
+    render() {
+        const contentStyle = {
+            backgroundColor: this.props.data.color,
+        }
         return (
-            <div>
-                <p>{this.props.data.content}</p>
-                <div>
-                    {childs}
+            <div className={'child'} style={this.createStyle()}>
+                <div className={'content'} style={contentStyle}>{this.props.data.content}</div>
+                <div className={'childContainer'}>
+                    {this.createChilds()}
                 </div>
             </div>
         )
