3490d333697efe335eeb27904052a34a20b09271
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-07 16:46:45 +0100

    Add NodeAdder component

--- app/App.js
+++ app/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 import values from './Tree'
 import TreeNode from './TreeNode'
+import NodeAdder from './NodeAdder'
 
 class App extends Component {
   render(){
     return (
-      <TreeNode data={values}/>
+        <div>
+            <TreeNode data={values}/>
+            <NodeAdder/>
+        </div>
     );
   }
 }
---
+++ app/NodeAdder.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Patrik Valkovic
+ * 11/7/17.
+ */
+
+'use strict'
+import React, {Component} from 'react'
+
+export default class NodeAdder extends Component {
+    render(){
+        return(
+            <div className={"adder"}>
+                Hello here
+            </div>
+        )
+    }
+}
