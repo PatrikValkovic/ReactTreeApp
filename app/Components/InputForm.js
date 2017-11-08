4d60d47fe03902649877e844761685f15b4bd297
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-08 09:46:08 +0100

    Solved clicking on child element hide whole form

--- app/Components/InputForm.js
+++ app/Components/InputForm.js
@@ -8,10 +8,15 @@ import React, {Component} from 'react'
 import actions from '../Flux/formActions'
 
 export default class InputForm extends Component {
+
+    static disableClick(event){
+        return event.stopPropagation()
+    }
+
     render() {
         return (
             <div className={'formBackground'} onClick={()=>actions.hideForm()}>
-                <div className={'formContainer'}>
+                <div className={'formContainer'} onClick={InputForm.disableClick}>
                     <input type="text"/>
                     <input type="color"/>
                     <input type="submit"/>
