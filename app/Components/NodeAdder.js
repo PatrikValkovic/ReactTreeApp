d50557acb22b89dc1ca5eef16b1cbaaa261807e2
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-08 12:01:14 +0100

    Solved transparent background

--- app/Components/NodeAdder.js
+++ app/Components/NodeAdder.js
@@ -10,10 +10,10 @@ import formActions from '../Flux/formActions'
 export default class NodeAdder extends Component {
     render() {
         return (
-            <a href="#" className={'addLink'}
-               onClick={formActions.showNewForm}>
+            <button className={'btn btn-success addLink'}
+                    onClick={formActions.showNewForm}>
                 &#10133;
-            </a>
+            </button>
         )
     }
 }
