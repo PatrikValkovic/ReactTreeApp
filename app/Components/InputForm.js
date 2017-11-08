1a08ab0d3931a7dfc923952ef91f0950a72e84b7
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-08 09:36:51 +0100

    Add grey overlay

--- app/Components/InputForm.js
+++ app/Components/InputForm.js
@@ -5,14 +5,17 @@
 
 'use strict'
 import React, {Component} from 'react'
+import actions from '../Flux/formActions'
 
-export default class InputForm extends Component{
-    render(){
+export default class InputForm extends Component {
+    render() {
         return (
-            <div>
-                <input type="text" />
-                <input type="color" />
-                <input type="submit" />
+            <div className={'formBackground'}>
+                <div className={'formContainer'}>
+                    <input type="text"/>
+                    <input type="color"/>
+                    <input type="submit"/>
+                </div>
             </div>
         )
     }
