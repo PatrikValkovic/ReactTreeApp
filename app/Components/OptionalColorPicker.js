c5d399004e3b1aa6e1d03077b5860e5407cf7e06
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-09 12:50:31 +0100

    Finished OptionalColorPicker

--- app/Components/OptionalColorPicker.js
+++ app/Components/OptionalColorPicker.js
@@ -6,10 +6,32 @@
 'use strict'
 
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 export default class OptionalColorPicker extends Component {
+    constructor(props) {
+        super(props)
+        this.defaultChanged = this.defaultChanged.bind(this)
+        this.colorChanged = this.colorChanged.bind(this)
+    }
+
+    defaultChanged() {
+        const fn = this.props.defaultChanged || (() => {
+            console.error('OptionalColorPicker.defaultChanged is not set')
+        })
+        fn()
+    }
+
+    colorChanged() {
+        const fn = this.props.colorChanged || (() => {
+            console.error('OptionalColorPicker.colorChanged is not set')
+        })
+        fn()
+    }
+
 
     render() {
+        const useDefault = this.props.useDefault || true
         return (
             <div className={'form-group optional-color-picker'}>
                 <label>
@@ -17,13 +39,22 @@ export default class OptionalColorPicker extends Component {
                 </label>
                 <div className={'checkbox-container'}>
                     <span>
-                        <input type="checkbox" checked={this.props.useDefaultValue || true}/>
-                        {this.props.defaultValueText || 'Use default value'}
+                        <input type="checkbox" checked={useDefault} onChange={this.defaultChanged}/>
+                        {this.props.defaultText || 'Use default value'}
                     </span>
                 </div>
                 <input type="color" name="color"
-                       className={'form-control'}/>
+                       className={'form-control'}
+                       onChange={this.colorChanged}
+                       disabled={useDefault}/>
             </div>
         )
     }
+}
+
+OptionalColorPicker.propTypes = {
+    useDefault: PropTypes.bool,
+    defaultText: PropTypes.string,
+    defaultChanged: PropTypes.func.isRequired,
+    colorChanged: PropTypes.func.isRequired,
 }
