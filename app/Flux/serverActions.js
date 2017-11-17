6fc7513027f7f2b30a825c345e805c0fa216d2eb
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-11-17 20:31:20 +0100

    Add cache into serverACtions

--- app/Flux/serverActions.js
+++ app/Flux/serverActions.js
@@ -10,15 +10,45 @@ import fetch from 'cross-fetch'
 import CONSTS from '../constants'
 import dispatch from '../dispatcher'
 
+const cache = {}
+
 const serverActions = {
 
+    saveToCache(hash, data){
+        cache[hash.toString()] = data
+    },
+
+    isInCache(hash){
+        return cache.hasOwnProperty(hash.toString())
+    },
+
+    getFromCache(hash){
+        if(this.isInCache(hash))
+            return cache[hash.toString()]
+        return null
+    },
+
     async loadData() {
+        let code = window.location.hash
+        if(code)
+            code = code.substr(1)
+        //try cache
+        if(this.isInCache(code)){
+            dispatch.dispatch({
+                type: CONSTS.ACTIONS.DATA_LOADED,
+                data: this.getFromCache(code),
+            })
+            return
+        }
+        //download data
         let url = CONSTS.SERVER.URL
-        const code = window.location.hash
         if (code)
-            url += `?${CONSTS.SERVER.REQ_NAME}=${code.substr(1)}`
+            url += `?${CONSTS.SERVER.REQ_NAME}=${code}`
         const response = await fetch(url)
         const data = await response.json()
+        //save downloaded data
+        this.saveToCache(code, data)
+        //dispatch event
         dispatch.dispatch({
             type: CONSTS.ACTIONS.DATA_LOADED,
             data,
@@ -39,12 +69,13 @@ const serverActions = {
             console.error("Cannot send data to server") //TODO
             return
         }
+        this.saveToCache(resp.url, data)
         window.history.pushState(null,'ReactTree', `#${resp.url}`)
     }
 }
 
-export default serverActions
-
-window.onhashchange = (hash) => {
+window.onhashchange = () => {
     serverActions.loadData()
 }
+
+export default serverActions
